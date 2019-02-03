67a2b096f82e869e902d82b813b61c33775e093c
Ruslan Murtazin <rouslanmurtazin@yandex.ru>
2019-02-03 19:44:50 +0300

    changed semestr start date

--- BotHost/Tools/WeeksCalculator.cs
+++ BotHost/Tools/WeeksCalculator.cs
@@ -7,14 +7,15 @@ namespace BotHost.Tools
 {
     public class WeeksCalculator
     {
-        private static DateTime _startSemestr = new DateTime(2018, 9, 2);
+        private static DateTime _startSemestr = new DateTime(2019, 2, 3);
         public static int GetCurrentWeek()
         {
+            // TODO Unit tests
             var days = (DateTime.Now - _startSemestr).Days;
             var week = days / 7;
             var tmp = days % 7;
             week = tmp > 0 ? week + 1 : week;
-            return week;
+            return week == 0 ? 1 : week;
         }
     }
 }
